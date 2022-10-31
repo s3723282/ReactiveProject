d12a57fe0a1b1a63d67b9d6f6e06315612a2fff7
s3723282 <s3723282@student.rmit.edu.au>
2022-10-31 13:46:38 +1100

    Working random path and random tree generation

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
+    public GameObject objectToSpawn;
     public static List<GameObject> GeneratedTiles = new List<GameObject>();
+    private List<Vector3> TileGenerator = new List<Vector3>();
 
     [SerializeField] private GameObject tilePrefab;
 
@@ -23,10 +25,14 @@ public class WorldGeneration : MonoBehaviour
                     new Vector3(x * 1f, 0, z * 1f),
                     Quaternion.identity);
 
+                tile.transform.SetParent(this.transform);
+
                 GeneratedTiles.Add(tile);
+                TileGenerator.Add(tile.transform.position);
                 pathGenerator.AssignTopAndBottomTiles(z, tile);
             }
         }
+        SpawnObject();
 
         //World Generated
         pathGenerator.GeneratePath();
@@ -36,5 +42,30 @@ public class WorldGeneration : MonoBehaviour
             pObject.SetActive(false);
         }
     }
-}
 
+    private void SpawnObject()
+    {
+        for (int c = 0; c < 20; c++)
+        {
+            GameObject toPlaceObject = Instantiate(objectToSpawn,
+            ObjectSpawnLocation(),
+            Quaternion.identity);
+        }
+    }
+
+    private Vector3 ObjectSpawnLocation()
+    {
+        int rndIndex = Random.Range(0, TileGenerator.Count);
+
+        Vector3 newPos = new Vector3(
+            TileGenerator[rndIndex].x,
+            TileGenerator[rndIndex].y + 0.5f,
+            TileGenerator[rndIndex].z
+            );
+
+        TileGenerator.RemoveAt(rndIndex);
+        return newPos;
+
+    }
+    
+}
